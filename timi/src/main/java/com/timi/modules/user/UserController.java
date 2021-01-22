f755152f8ab7717b0d8ef6bc7ed4070c3f857bf8
huhonghao <18321456123@163.com>
2021-01-22 17:30:34 +0800

    nacos

--- timi/src/main/java/com/timi/modules/user/UserController.java
+++ timi/src/main/java/com/timi/modules/user/UserController.java
@@ -1,5 +1,9 @@
 package com.timi.modules.user;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -9,9 +13,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("user")
 public class UserController {
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+    @NacosValue("${email.server.address}")
+    private String emailServer;
+
     @GetMapping("query")
     public String test(){
-        System.out.println("123");
-        return "This is a Test";
+        String supperUserName = applicationContext.getEnvironment().getProperty("supperUserName");
+        String st=supperUserName+"-------------"+emailServer;
+        return st;
     }
 }
--- timi/src/test/java/com/timi/TimiApplicationTests.java
+++ timi/src/test/java/com/timi/TimiApplicationTests.java
@@ -1,12 +1,12 @@
 package com.timi;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TimiApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
